e86179a0c3410f8a44c603eac809c745b805ecc3
Ben Huang <benjaminwong1985@gmail.com>
2024-09-14 17:40:19 -0400

    learn: first scene and imported mordel

--- src/babylon/SceneComponent.jsx
+++ src/babylon/SceneComponent.jsx
@@ -1,64 +1,57 @@
 import { useEffect, useRef } from "react";
-import { Engine, Scene } from "@babylonjs/core";
-
-export default function Main({
-  antialias,
-  engineOptions,
-  adaptToDeviceRatio,
-  sceneOptions,
-  onRender,
-  onSceneReady,
-  ...rest
-}) {
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  MeshBuilder,
+  SceneLoader,
+} from "@babylonjs/core";
+export default function Main() {
   const reactCanvas = useRef(null);
 
-  // set up basic engine and scene
   useEffect(() => {
     const { current: canvas } = reactCanvas;
-
-    if (!canvas) return;
-
-    const engine = new Engine(
-      canvas,
-      antialias,
-      engineOptions,
-      adaptToDeviceRatio,
-    );
-    const scene = new Scene(engine, sceneOptions);
-    if (scene.isReady()) {
-      onSceneReady(scene);
+    if (!canvas) {
+      return;
     } else {
-      scene.onReadyObservable.addOnce((scene) => onSceneReady(scene));
+      canvas.width = 0.9 * window.innerWidth;
+      canvas.height = 0.9 * window.innerHeight;
+      const engine = new Engine(canvas, true);
+      const scene = new Scene(engine);
+      const camera = new ArcRotateCamera(
+        "camera",
+        -Math.PI / 2,
+        Math.PI / 2.5,
+        3,
+        new Vector3(0, 0, 0),
+        scene,
+      );
+      camera.attachControl(canvas, true);
+      const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+      const box = MeshBuilder.CreateBox("box", {}, scene);
+      SceneLoader.ImportMeshAsync(
+        "semi_house",
+        "https://assets.babylonjs.com/meshes/",
+        "both_houses_scene.babylon",
+      ).then((res) => {
+        const semiHouse = res.meshes[0];
+        semiHouse.position.z = 3;
+        semiHouse.rotation.y = Math.PI / 3;
+      });
+      engine.runRenderLoop(() => {
+        scene.render();
+      });
+      window.addEventListener("resize", () => {
+        engine.resize();
+      });
     }
+  }, []);
 
-    engine.runRenderLoop(() => {
-      if (typeof onRender === "function") onRender(scene);
-      scene.render();
-    });
-
-    const resize = () => {
-      scene.getEngine().resize();
-    };
-
-    if (window) {
-      window.addEventListener("resize", resize);
-    }
-
-    return () => {
-      scene.getEngine().dispose();
-
-      if (window) {
-        window.removeEventListener("resize", resize);
-      }
-    };
-  }, [
-    antialias,
-    engineOptions,
-    adaptToDeviceRatio,
-    sceneOptions,
-    onRender,
-    onSceneReady,
-  ]);
-
-  return <canvas ref={reactCanvas} {...rest} />;
+  return (
+    <div>
+      <canvas ref={reactCanvas} />
+    </div>
+  );
 }
